60f63d858e6f42fa3aff2a1de35d41606666faf9
Michael Kibenko <michaelk@bio-nexus.com>
2019-11-21 09:30:46 +0200

    Rebuild the deployment code

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash          
-client_id="523893b6-bfeb-489f-a5d9-e2f6e0e9f94f"
-client_secret="dHMeTVH=mZeE:4x:3YF=m1tA9nRnn@p."
-subscription_id="f3e0167f-3e80-42f1-a42f-c8abc0655c11"
-tenant_id="700123f5-16bf-4143-a6a6-d0bf7526e03d"
+client_id=""
+client_secret=""
+subscription_id=""
+tenant_id=""
 image_name="EchoServerImage"
-ressource_group="packerDemoRessourceGroup"
+ressource_group=""
 location="West Europe"
 
 #build packer image
