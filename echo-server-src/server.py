a5cbef2b0583517b18a3ecba82ccccdae1c0d76c
Michael Kibenko <michaelk@bio-nexus.com>
2019-11-21 23:49:11 +0200

    back the ip config

--- echo-server-src/server.py
+++ echo-server-src/server.py
@@ -1,9 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 
-import socket
-hostname = socket.gethostname()
-ip = socket.gethostbyname()
+# import socket
+# hostname = socket.gethostname()
+# ip = socket.gethostbyname(hostname)
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -12,7 +12,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(ip)
         response.write(b': Received:')
         response.write(body)
         self.wfile.write(response.getvalue())
