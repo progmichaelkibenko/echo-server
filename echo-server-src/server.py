0786cfe37e8a4e7e7f60db957eea8741b6282879
Michael Kibenko <michaelk@bio-nexus.com>
2019-11-23 13:22:37 +0200

    complete the ip address prefix

--- echo-server-src/server.py
+++ echo-server-src/server.py
@@ -1,9 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
-
-# import socket
-# hostname = socket.gethostname()
-# ip = socket.gethostbyname(hostname)
+import socket
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+ipBytes = ip.encode()
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -12,10 +12,10 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(b': Received:')
+        response.write(ipBytes)
+        response.write(b': Says:')
         response.write(body)
         self.wfile.write(response.getvalue())
-
 
 print('Server listening on port 80...')
 httpd = HTTPServer(('0.0.0.0', 80), Handler)
